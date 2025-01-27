0861230a8a2054c8c62025229b0d96f289abf776
Kevin Schuab <kevinschuab@gmail.com>
2025-01-27 14:08:52 -0300

    BugFix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className="text-5xl font-bold">Seja Bem Vindo!</h2>
-            <p className="text-xl">Compre aqui seus remédios</p>
+            <p className="text-xl">Compre aqui seus remédios 💊</p>
 
             <div className="flex justify-around gap-4">
               <div
